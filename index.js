b26cf667501bc8d342c21a2cf155d04bfb93fab5
web3 vicky <vicky.web3@gmail.com>
2022-07-17 13:48:51 +0530

    commit

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-function Card(props) {
+const data = [
+    {
+        id: 1,
+        title: "Life Lessons with Katie Zaferes",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "./katie-zaferes.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "./wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "./mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    }
+]
+
+
+
+
+function Cards(props) {
+
+
     return(
-    <div className="Card">
-        <img src={props.img} alt="Katie Zaferes" className="card-katie" />
+    <div className="Cards">
+        <img src={props.img} className="card-img" />
         <div className="card-stats">
             <img src="./star.png" className="card-star" /> 
             <span>{props.rating} </span>
             <span className="gray">({props.upvotes}) ▪️ </span>
             <span className="gray"> {props.country}</span>
         </div>  
-        <p>{props.subject}</p>
+        <p>{props.title}</p>
         <p><span className="bold">From ${props.price}</span> / person</p>
     </div>
     )
@@ -34,18 +81,21 @@ function NavBar() {
 }
 
 function App() {
+    const cards = data.map(item => {
+        return <Cards
+                    img={item.coverImg}
+                    rating={item.stats.rating}
+                    upvotes={item.stats.reviewCount}
+                    country={item.location}
+                    title={item.title}
+                    price={item.price}                    
+                />
+    })
     return(
         <div className="App">
             <NavBar />
             <Hero />
-            <Card 
-            img="./katie-zaferes.png"
-            rating="5.0"
-            upvotes={6}
-            country="USA"
-            subject="Life Lessons with Katie Zaferes"
-            price={136}
-            />
+            {cards}
         </div>
     )
 }
